d22a5bb67df8fa2f02d3b2a2c20f5ac2c71ae18e
DinkoIlicic <ilicicdinko@gmail.com>
2019-01-16 20:37:36 +0100

    Test

--- 01_Homework/hw1.php
+++ 01_Homework/hw1.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: dinko
- * Date: 16.01.19.
- * Time: 20:19
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homework 01</title>
+</head>
+<body bgcolor="<?php echo $_GET['colour'];?>">
+<form action="hw1.php" method="get">
+    Colour: <input type="text" name="colour">
+    <input type="submit">
+
+
+</form>
+
